4f7dca902b7ba7fb39d99dc42d00866c720d3dce
soldierenkov <anil@rambler.ru>
2018-01-05 17:48:05 +0200

    first step

---
+++ assets/js/application.js
@@ -0,0 +1,51 @@
+/*
+	Andrei Soldatenkov
+	WebClient testing:
+*/
+var exampleSocket = new WebSocket("ws://192.10.1.37:5880");
+console.log("server started");
+exampleSocket.onmessage = function (event) {
+    console.log(event.data);
+    $( "#dough01" ).text(event.data);
+    //document.getElementById("myLog").value = event.data;
+
+  };
+
+
+(function($) {
+
+    var $bt_sendMessage = $('dough')
+    $(function() {
+
+        $( "a.dough" ).click(function() {
+            alert( "Send message to server via WebSocket." );
+            console.log("send message");
+            var msg = {
+                type: "message",
+                text: this.text,
+                id:   123456789,
+                date: Date.now()
+              };
+            
+              // Send the msg object as a JSON-formatted string.
+              exampleSocket.send(JSON.stringify(msg));
+              $( "#curDough" ).text(this.text);
+              $("a.dough").css("background-color", "transparent");
+              $(this).css("background", "yellow");
+
+          });
+
+        $bt_sendMessage.on('click', 'a', function(event) {
+            console.log("send message");
+            var msg = {
+                type: "message",
+                text: "I love you",
+                id:   123456789,
+                date: Date.now()
+              };
+            
+              // Send the msg object as a JSON-formatted string.
+              exampleSocket.send(JSON.stringify(msg));
+        });
+    });   
+})(jQuery);
